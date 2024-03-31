c9e5c03b2087ec14ef13145ce4c157859edf7821
DoGiaKhang <164725044+DoGiaKhang@users.noreply.github.com>
2024-03-31 15:05:48 +0700

    python l6 bai 2

---
+++ PythonL6-Bai2.py
@@ -0,0 +1,9 @@
+
+#Bai2
+print("Vung oi, ... mo ra")
+password = input("Nhap mat khau: ")
+while password != "python":
+    print("mat khau sai")
+    password = input("Yeu cau nhap lai mat khau: ")
+else:
+    print("xin chao")
